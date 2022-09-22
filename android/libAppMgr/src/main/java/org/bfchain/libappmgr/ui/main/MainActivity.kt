2235fcfc27ca60bc881f1cd8c1768dc9caec6dd5
jackie.huang <hl19081555@gmail.com>
2022-09-22 18:16:28 +0800

    修复第一次加载时，asset未迁移完成导致的加载问题

--- android/libAppMgr/src/main/java/org/bfchain/libappmgr/ui/main/MainActivity.kt
+++ android/libAppMgr/src/main/java/org/bfchain/libappmgr/ui/main/MainActivity.kt
@@ -1,40 +1,30 @@
 package org.bfchain.libappmgr.ui.main
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import org.bfchain.libappmgr.data.PreferencesHelper
 import org.bfchain.libappmgr.entity.AppInfo
-import org.bfchain.libappmgr.entity.AppVersion
-import org.bfchain.libappmgr.entity.AutoUpdateInfo
-import org.bfchain.libappmgr.network.base.IApiResult
 import org.bfchain.libappmgr.schedule.CoroutineUpdateTask
-import org.bfchain.libappmgr.ui.download.DownLoadViewModel
 import org.bfchain.libappmgr.ui.theme.AppMgrTheme
 import org.bfchain.libappmgr.ui.view.AppInfoGridView
 import org.bfchain.libappmgr.utils.FilesUtil
-import java.io.File
 
 class MainActivity : ComponentActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-
-    GlobalScope.launch {
+    /*GlobalScope.launch {
       // 判断是否是第一次加载软件，如果是，那么将assets中的remember-app拷贝到app目录下
       if (PreferencesHelper.isFirstIn()) {
         FilesUtil.copyAssetsToRecommendAppDir()
@@ -42,7 +32,7 @@ class MainActivity : ComponentActivity() {
       }
 
       CoroutineUpdateTask().scheduleUpdate(1000 * 60)
-    }
+    }*/
 
     setContent {
       AppMgrTheme {
@@ -51,27 +41,32 @@ class MainActivity : ComponentActivity() {
             .fillMaxSize()
             .background(MaterialTheme.colors.primary)
         ) {
-          Gretting(name = "Compose!!", this@MainActivity)
-          // ProgressCircular( progressValue.value)
-          // Test()
+          Home()
+          //Gretting(name = "Compose!!")
         }
       }
     }
   }
 }
 
-@SuppressLint(
-  "UnrememberedMutableState", "FlowOperatorInvokedInComposition",
-  "CoroutineCreationDuringComposition"
-)
 @Composable
-fun Gretting(name: String, context: Context) {
-  // Text(text = "Hello $name")
+fun Gretting(name: String) {
+  Text(text = "Hello $name")
+}
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun Home() {
   var appInfoList: MutableList<AppInfo> = mutableStateListOf()
   LaunchedEffect(Unit) {
+    if (PreferencesHelper.isFirstIn()) {
+      FilesUtil.copyAssetsToRecommendAppDir()
+      PreferencesHelper.saveFirstState(false)
+    }
     // 拷贝完成后，通过app目录下的remember-app和system-app获取最新列表数据
     appInfoList.clear()
     appInfoList.addAll(FilesUtil.getAppInfoList())
+    CoroutineUpdateTask().scheduleUpdate(1000 * 60) // 轮询执行
   }
   AppInfoGridView(appInfoList = appInfoList, downModeDialog = false)
 }
