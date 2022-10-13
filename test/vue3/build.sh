b6c8db099fd048c4ae9d65d5d0d02e3e356892fb
kingsword09 <kingsword09@gmail.com>
2022-10-13 16:16:23 +0800

    :fire: 修改install为bundle，废弃@bfsx/install改为@bfsx/bundle

--- test/vue3/build.sh
+++ test/vue3/build.sh
@@ -20,7 +20,7 @@ then
     rm -rf ./dist/*
     tar -xvf KEJPMHLA.bfsa -C ./dist
 else
-    echo "you need bundle @bfsx/bundle run  npm install -g @bfsx/bundle ,then try again."
+    echo "you need install @bfsx/bundle run  npm install -g @bfsx/bundle ,then try again."
 fi
 
 mv KEJPMHLA.bfsa ./dist && mv appversion.json ./dist
